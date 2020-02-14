c2a8ae4ff86c8fc90bc24b1941fe3d2717234b09
Zhihao Ruan <ruanzh@umich.edu>
2020-02-14 10:07:21 -0500

    reformat

--- deadlock.cpp
+++ deadlock.cpp
@@ -1,41 +1,38 @@
 #include <iostream>
-#include <thread>
 #include <mutex>
+#include <thread>
 
 std::mutex x, y, c;
 
-void child1()
-{
-    for (int i=0; i<1000000; i++) {
-        if (!(i%100)) {
-	    c.lock();
-	    std::cout << "child1: " << i << std::endl;
-	    c.unlock();
-	}
-	x.lock();
-	y.lock();
-	y.unlock();
-	x.unlock();
+void child1() {
+    for (int i = 0; i < 1000000; i++) {
+        if (!(i % 100)) {
+            c.lock();
+            std::cout << "child1: " << i << std::endl;
+            c.unlock();
+        }
+        x.lock();
+        y.lock();
+        y.unlock();
+        x.unlock();
     }
 }
 
-void child2()
-{
-    for (int i=0; i<1000000; i++) {
-        if (!(i%100)) {
-	    c.lock();
-	    std::cout << "child2: " << i << std::endl;
-	    c.unlock();
-	}
-	y.lock();
-	x.lock();
-	x.unlock();
-	y.unlock();
+void child2() {
+    for (int i = 0; i < 1000000; i++) {
+        if (!(i % 100)) {
+            c.lock();
+            std::cout << "child2: " << i << std::endl;
+            c.unlock();
+        }
+        y.lock();
+        x.lock();
+        x.unlock();
+        y.unlock();
     }
 }
 
-int main()
-{
+int main() {
     std::thread* t[2];
 
     std::thread t1(child1);
